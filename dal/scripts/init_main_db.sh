f75c5ee8547947b11431dbafb6cfe319ce049890
peinlcy <peinlcy@gmail.com>
2023-12-01 19:03:44 +0800

    update database url

--- dal/scripts/init_main_db.sh
+++ dal/scripts/init_main_db.sh
@@ -20,7 +20,7 @@ DB_USER=${POSTGRES_USER:=admin}
 # Check if a custom password has been set, otherwise default to 'password'
 DB_PASSWORD="${POSTGRES_PASSWORD:=admin123}"
 # Check if a custom database name has been set, otherwise default to 'olaos'
-DB_NAME="${POSTGRES_DB:=olaos_main}"
+DB_NAME="${POSTGRES_DB:=olaos}"
 # Check if a custom port has been set, otherwise default to '5432'
 DB_PORT="${POSTGRES_PORT:=5432}"
 # Check if a custom repl password has been set, otherwise default to 'repl123'
