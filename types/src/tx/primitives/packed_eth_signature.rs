b1c1d2999f524ddf4eae632659dcc0c86dad8b9e
peinlcy <peinlcy@gmail.com>
2023-12-11 13:06:07 +0800

    add transaction request test

--- types/src/tx/primitives/packed_eth_signature.rs
+++ types/src/tx/primitives/packed_eth_signature.rs
@@ -1,6 +1,10 @@
-use ola_basic_types::H256;
+use ethereum_types_old::H256 as ParityCryptoH256;
+use ola_basic_types::{Address, H256};
 use ola_utils::hash::hash_bytes;
-use parity_crypto::publickey::Signature as ETHSignature;
+use parity_crypto::publickey::{
+    public_to_address, recover, sign, Error as ParityCryptoError, KeyPair,
+    Signature as ETHSignature,
+};
 
 use super::{EIP712TypedStructure, Eip712Domain};
 /// Struct used for working with ethereum signatures created using eth_sign (using geth, ethers.js, etc)
@@ -24,6 +28,16 @@ use super::{EIP712TypedStructure, Eip712Domain};
 pub struct PackedEthSignature(ETHSignature);
 
 impl PackedEthSignature {
+    pub fn r(&self) -> &[u8] {
+        self.0.r()
+    }
+    pub fn s(&self) -> &[u8] {
+        self.0.s()
+    }
+    pub fn v(&self) -> u8 {
+        self.0.v()
+    }
+
     pub fn typed_data_to_signed_bytes(
         domain: &Eip712Domain,
         typed_struct: &impl EIP712TypedStructure,
@@ -33,5 +47,27 @@ impl PackedEthSignature {
         bytes.extend_from_slice(domain.hash_struct().as_bytes());
         bytes.extend_from_slice(typed_struct.hash_struct().as_bytes());
         hash_bytes(&bytes)
+    }
+
+    pub fn message_to_signed_bytes(msg: &[u8]) -> H256 {
+        hash_bytes(msg)
+    }
+
+    pub fn sign_raw(
+        private_key: &H256,
+        signed_bytes: &H256,
+    ) -> Result<PackedEthSignature, ParityCryptoError> {
+        let private_key = ParityCryptoH256::from_slice(&private_key.0);
+        let signed_bytes = ParityCryptoH256::from_slice(&signed_bytes.0);
+
+        let secret_key = private_key.into();
+        let signature = sign(&secret_key, &signed_bytes)?;
+        Ok(PackedEthSignature(signature))
+    }
+
+    pub fn address_from_private_key(private_key: &H256) -> Result<Address, ParityCryptoError> {
+        let private_key = ParityCryptoH256::from_slice(&private_key.0);
+        let address = KeyPair::from_secret(private_key.into())?.address();
+        Ok(Address::from(address.0))
     }
 }
