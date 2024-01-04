3a5e286ded37cc6d66e3222a91076fb16a34535f
web3Softcloud <renzhexin@web3.com>
2024-01-04 13:57:55 +0800

    ADD: from-key to generate keystore in cli.

--- client/cli/src/subcommands/signer/keystore/mod.rs
+++ client/cli/src/subcommands/signer/keystore/mod.rs
@@ -10,6 +10,9 @@ use inspect_private::InspectPrivate;
 mod inspect;
 use inspect::Inspect;
 
+mod from_key;
+use from_key::FromKey;
+
 #[derive(Debug, Parser)]
 pub struct Keystore {
     #[clap(subcommand)]
@@ -20,8 +23,12 @@ pub struct Keystore {
 enum Subcommands {
     #[clap(about = "Randomly generate a new keystore")]
     New(New),
+    #[clap(about = "Check the private key of an existing keystore file")]
     InspectPrivate(InspectPrivate),
+    #[clap(about = "Check the public key of an existing keystore file")]
     Inspect(Inspect),
+    #[clap(about = "Create a keystore file from an existing private key")]
+    FromKey(FromKey),
 }
 
 impl Keystore {
@@ -30,6 +37,7 @@ impl Keystore {
             Subcommands::New(cmd) => cmd.run(),
             Subcommands::InspectPrivate(cmd) => cmd.run(),
             Subcommands::Inspect(cmd) => cmd.run(),
+            Subcommands::FromKey(cmd) => cmd.run(),
         }
     }
 }
