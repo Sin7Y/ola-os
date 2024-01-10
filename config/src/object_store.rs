7a395643a500e80c8001a6dda04c997cf53ae5c9
peinlcy <peinlcy@gmail.com>
2024-01-10 16:00:18 +0800

    add file object_store

---
+++ config/src/object_store.rs
@@ -0,0 +1,16 @@
+use serde::Deserialize;
+
+#[derive(Debug, Deserialize, Eq, PartialEq, Clone, Copy)]
+pub enum ObjectStoreMode {
+    FileBacked,
+}
+
+/// Configuration for the object store
+#[derive(Debug, Deserialize, Clone, PartialEq)]
+pub struct ObjectStoreConfig {
+    pub bucket_base_url: String,
+    pub mode: ObjectStoreMode,
+    pub file_backed_base_path: String,
+    pub gcs_credential_file_path: String,
+    pub max_retries: u16,
+}
