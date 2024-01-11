8945e2971482b9decfe06412c320920dcb4e1066
peinlcy <peinlcy@gmail.com>
2024-01-11 16:29:15 +0800

    add proof_data_handler config

---
+++ config/src/proof_data_handler.rs
@@ -0,0 +1,22 @@
+use std::time::Duration;
+
+use serde::Deserialize;
+
+#[derive(Debug, Deserialize, Clone, Copy, PartialEq)]
+pub enum ProtocolVersionLoadingMode {
+    FromDb,
+    FromEnvVar,
+}
+
+#[derive(Debug, Deserialize, Clone, PartialEq)]
+pub struct ProofDataHandlerConfig {
+    pub http_port: u16,
+    pub proof_generation_timeout_in_secs: u16,
+    pub protocol_version_loading_mode: ProtocolVersionLoadingMode,
+    pub fri_protocol_version_id: u16,
+}
+impl ProofDataHandlerConfig {
+    pub fn proof_generation_timeout(&self) -> Duration {
+        Duration::from_secs(self.proof_generation_timeout_in_secs as u64)
+    }
+}
