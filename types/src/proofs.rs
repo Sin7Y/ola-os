f6a5865982d2436a7aa11dea9dd877e079b60fd2
peinlcy <peinlcy@gmail.com>
2024-01-18 11:28:18 +0800

    add enum AggregationRound

---
+++ types/src/proofs.rs
@@ -0,0 +1,58 @@
+use std::str::FromStr;
+
+/// Represents the sequential number of the proof aggregation round.
+/// Mostly used to be stored in `aggregation_round` column  in `prover_jobs` table
+#[derive(Debug, Clone, Copy, PartialEq, Eq, Hash, serde::Serialize, serde::Deserialize)]
+pub enum AggregationRound {
+    BasicCircuits = 0,
+}
+
+impl From<u8> for AggregationRound {
+    fn from(item: u8) -> Self {
+        match item {
+            0 => AggregationRound::BasicCircuits,
+            _ => panic!("Invalid round"),
+        }
+    }
+}
+
+impl AggregationRound {
+    pub fn next(&self) -> Option<AggregationRound> {
+        match self {
+            AggregationRound::BasicCircuits => None,
+        }
+    }
+}
+
+impl std::fmt::Display for AggregationRound {
+    fn fmt(&self, formatter: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        formatter.write_str(match self {
+            Self::BasicCircuits => "basic_circuits",
+        })
+    }
+}
+
+impl FromStr for AggregationRound {
+    type Err = String;
+
+    fn from_str(s: &str) -> Result<Self, Self::Err> {
+        match s {
+            "basic_circuits" => Ok(AggregationRound::BasicCircuits),
+            other => Err(format!(
+                "{} is not a valid round name for witness generation",
+                other
+            )),
+        }
+    }
+}
+
+impl TryFrom<i32> for AggregationRound {
+    type Error = ();
+
+    fn try_from(v: i32) -> Result<Self, Self::Error> {
+        match v {
+            x if x == AggregationRound::BasicCircuits as i32 => Ok(AggregationRound::BasicCircuits),
+            _ => Err(()),
+        }
+    }
+}
