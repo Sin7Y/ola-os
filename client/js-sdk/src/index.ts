05d5b28fde7c7bc4ffb5f1f02f4c79838b20c038
Bob Niu <spartucus@users.noreply.github.com>
2024-01-27 23:52:05 +0800

    progressing

--- client/js-sdk/src/index.ts
+++ client/js-sdk/src/index.ts
@@ -3,4 +3,4 @@ export { OlaSigner } from "./signer";
 export { OlaProvider } from "./provider";
 export { encodeAbi, decodeAbi } from "./abi";
 export { U256, H256 } from "./types";
-export { TransactionType, L2Tx, parseTx, encodeTransaction } from "./example";
+export { TransactionType, L2Tx, parseTx, encodeTransaction, createCalldata, createTransactionRaw, Address } from "./example";
