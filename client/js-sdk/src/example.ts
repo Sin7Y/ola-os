d8c111d70f79bf2bd029ef76b0f606a7af55df41
Bob Niu <spartucus@users.noreply.github.com>
2024-01-27 15:53:14 +0800

    progressing: add h256/u256 types

--- client/js-sdk/src/example.ts
+++ client/js-sdk/src/example.ts
@@ -1,19 +1,17 @@
+import { Signature } from "ethers";
 import { encode_input_from_js } from "./crypto/ola_lang_abi"
-
-type H256 = string;
+import { U256, H256 } from "./types";
+import { OlaSigner } from "./signer";
+import { RLP } from "@ethereumjs/rlp";
 
 type Address = H256;
 
-const ENTRYPOINT_ADDRESS = "0x8001";
+const ENTRYPOINT_ADDRESS: H256 = H256.from("0x8001");
 
 interface Execute {
     contract_address: Address;
     calldata: number[];
     factory_deps?: number[][] | null;
-}
-
-interface Nonce {
-    nonce:number;
 }
 
 interface InputData {
@@ -30,7 +28,7 @@ export enum TransactionType {
 }
 
 interface L2TxCommonData {
-    nonce: Nonce;
+    nonce: U256;
     initiator_address: Address;
     signature: number[];
     transaction_type: TransactionType;
@@ -43,9 +41,11 @@ export interface L2Tx {
     received_timestamp_ms: number;
 }
 
-type U256 = [number, number, number, number];
-
 type U64 = [number];
+
+type Option<T> = T | null;
+
+type SignatureTuple = [Option<U64>, Option<U256>, Option<U256>];
 
 interface Eip712Meta {
     factory_deps?: number[][] | null;
@@ -72,18 +72,63 @@ interface TransactionRequest {
     chain_id?: number | null;
 }
 
-// export function createTransactionRaw(l2tx: L2Tx): number[] {
+export function createTransactionRaw(l2tx: L2Tx, signer: OlaSigner): number[] {
+    // TODO: chainid should be extract from common_data
+    let chain_id = 1027;
+    let tx_type = l2tx.common_data.transaction_type;
+    let r = l2tx.common_data.signature.slice(0,32).map((byte) => byte.toString(16).padStart(2, "0")).join("");
+    let s = l2tx.common_data.signature.slice(32,64).map((byte) => byte.toString(16).padStart(2, "0")).join("");
+    let tx_req: TransactionRequest = {
+        nonce: l2tx.common_data.nonce,
+        from: l2tx.common_data.initiator_address,
+        input: l2tx.execute.calldata,
+        v: [0],
+        r: U256.from(r),
+        s: U256.from(s),
+        transaction_type: tx_type,
+        chain_id: chain_id,
+    };
+    switch (tx_type) {
+        case TransactionType.EIP712Transaction:
+            tx_req.eip712_meta = {
+                factory_deps: l2tx.execute.factory_deps,
+                custom_signature: l2tx.common_data.signature,
+                paymaster_params: null
+            };
+            break;
+        case TransactionType.EIP1559Transaction:
+            break;
+        case TransactionType.OlaRawTransaction:
+            tx_req.eip712_meta = {
+                factory_deps: l2tx.execute.factory_deps,
+                custom_signature: l2tx.common_data.signature,
+                paymaster_params: null
+            };
+        default:
+            throw new Error("Invalid transaction type: " + tx_type);
+            break;
+    }
 
-// }
+    let signature = signTransactionRequest(signer, tx_req);
+
+    let tobeEncode: Buffer[] = [];
+    switch (tx_type) {
+        case TransactionType.EIP1559Transaction:
+            // tobeEncode.push(Buffer.from(chain_id));
+            break;
+    }
+
+    return [1]
+}
 
 // TODO: use ECDSA sign the TransactionRequest.
-function signTransactionRequest(transaction_request: TransactionRequest): number[] {
+function signTransactionRequest(signer: OlaSigner, transaction_request: TransactionRequest): number[] {
     return [1, 2];
 }
 
-export function encodeTransaction(chain_id: number, from: Address, nonce: Nonce, calldata: number[], factory_deps: number[][] | null): L2Tx {
+export function encodeTransaction(signer: OlaSigner, chain_id: number, from: Address, nonce: U256, calldata: number[], factory_deps: number[][] | null): L2Tx {
     let req: TransactionRequest = {
-        nonce: [0, 0, 0, nonce.nonce],
+        nonce: nonce,
         from: from,
         to: ENTRYPOINT_ADDRESS,
         input: calldata,
@@ -93,7 +138,7 @@ export function encodeTransaction(chain_id: number, from: Address, nonce: Nonce,
         },
         chain_id: chain_id
     };
-    let signature = signTransactionRequest(req);
+    let signature = signTransactionRequest(signer, req);
 
     let tx: L2Tx = {
         execute: {
--- client/js-sdk/src/index.ts
+++ client/js-sdk/src/index.ts
@@ -2,4 +2,5 @@ export { OlaWallet } from "./wallet";
 export { OlaSigner } from "./signer";
 export { OlaProvider } from "./provider";
 export { encodeAbi, decodeAbi } from "./abi";
+export { U256, H256 } from "./types";
 export { TransactionType, L2Tx, parseTx, encodeTransaction } from "./example";
