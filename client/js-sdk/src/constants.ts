1f4bb4f86c5954a821d8d543d5f8cdd73a40f421
olavm-k <k@olavm.org>
2024-02-01 00:01:32 +0800

    feat: create transaction

--- client/js-sdk/src/constants.ts
+++ client/js-sdk/src/constants.ts
@@ -1,1 +1,9 @@
 export const ENTRYPOINT_ADDRESS = [0x0n, 0x0n, 0x0n, 0x8001n];
+export const DEFAULT_CHAIN_ID = 1027;
+
+/**
+ * for setPubkey only
+ *
+ * 0x0000000000000000000000000000000000000000000000000000000000008006
+ */
+export const DEFAULT_ACCOUNT_ADDRESS = [0x0n, 0x0n, 0x0n, 0x8006n];
