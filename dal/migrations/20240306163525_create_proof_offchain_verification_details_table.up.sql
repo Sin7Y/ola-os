cf4013873cae3203544bee15efa8a39cf68438f9
peinlcy <peinlcy@gmail.com>
2024-03-08 16:02:00 +0800

    feat: update proof verify table

--- dal/migrations/20240306163525_create_proof_offchain_verification_details_table.up.sql
+++ dal/migrations/20240306163525_create_proof_offchain_verification_details_table.up.sql
@@ -1,15 +1,14 @@
--- Add up migration script here
 CREATE TABLE IF NOT EXISTS proof_offchain_verification_details
 (
     l1_batch_number         BIGINT PRIMARY KEY REFERENCES l1_batches (number) ON DELETE CASCADE,
     status                  TEXT      NOT NULL,
+    verifier_picked_at      TIMESTAMP,
+    verifier_submit_at      TIMESTAMP,
     created_at              TIMESTAMP NOT NULL,
-    updated_at              TIMESTAMP NOT NULL,
-    verifier_taken_at       TIMESTAMP
+    updated_at              TIMESTAMP NOT NULL
 );
 
 
-CREATE INDEX IF NOT EXISTS idx_proof_offchain_verification_details_status_verifier_taken_at
-    ON proof_offchain_verification_details (verifier_taken_at)
-    WHERE status = 'picked_by_verifier';
-
+CREATE INDEX IF NOT EXISTS idx_proof_offchain_verification_details_status_verifier_picked_at
+    ON proof_offchain_verification_details (verifier_picked_at)
+    WHERE status = 'picked_by_offchain_verifier';
