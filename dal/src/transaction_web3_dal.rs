fd64108f9c0734885fe99f3a8ad44b9956013eeb
Paul Cheng <chengyuejia@foxmail.com>
2024-04-23 14:26:23 +0800

    fix(dal): fix method in dal

--- dal/src/transaction_web3_dal.rs
+++ dal/src/transaction_web3_dal.rs
@@ -228,13 +228,10 @@ impl TransactionsWeb3Dal<'_, '_> {
                 transactions.l1_batch_tx_index AS l1_batch_tx_index,
                 transactions.miniblock_number AS "block_number!",
                 transactions.error AS error,
-                transactions.effective_gas_price AS effective_gas_price,
                 transactions.initiator_address AS initiator_address,
                 transactions.data -> 'to' AS "transfer_to?",
                 transactions.data -> 'contractAddress' AS "execute_contract_address?",
                 transactions.tx_format AS "tx_format?",
-                transactions.refunded_gas AS refunded_gas,
-                transactions.gas_limit AS gas_limit,
                 miniblocks.hash AS "block_hash",
                 miniblocks.l1_batch_number AS "l1_batch_number?",
                 sl.key AS "contract_address?"
@@ -265,29 +262,11 @@ impl TransactionsWeb3Dal<'_, '_> {
             .get_logs_by_tx_hashes(hashes)
             .await?;
 
-        let mut l2_to_l1_logs = self
-            .storage
-            .events_dal()
-            .get_l2_to_l1_logs_by_hashes(hashes)
-            .await?;
-
         for receipt in &mut receipts {
             let logs_for_tx = logs.remove(&receipt.transaction_hash);
 
             if let Some(logs) = logs_for_tx {
                 receipt.logs = logs
-                    .into_iter()
-                    .map(|mut log| {
-                        log.block_hash = Some(receipt.block_hash);
-                        log.l1_batch_number = receipt.l1_batch_number;
-                        log
-                    })
-                    .collect();
-            }
-
-            let l2_to_l1_logs_for_tx = l2_to_l1_logs.remove(&receipt.transaction_hash);
-            if let Some(l2_to_l1_logs) = l2_to_l1_logs_for_tx {
-                receipt.l2_to_l1_logs = l2_to_l1_logs
                     .into_iter()
                     .map(|mut log| {
                         log.block_hash = Some(receipt.block_hash);
